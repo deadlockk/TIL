999f5679a7333a295b4541f1dbcb33f9f243c6ea
SANG WON <style8914@naver.com>
2018-11-14 00:00:51 +0900

    Add files via upload

---
+++ Algorithm/구구단_2739.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <ios>
+
+using namespace std;
+
+int main() {
+	cin.tie(NULL);
+	cout.tie(NULL);
+	ios::sync_with_stdio(false);
+
+	int input=0;
+	cin >> input;
+	for(int i=1;i<=9;i++){
+		cout << input <<" * "<<i<<" = "<<input * i<< "\n";
+	}
+	return 0;
+}
