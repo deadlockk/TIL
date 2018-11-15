8aafad49b95bda29421f3b450615e52efe97f1f9
SANG WON <style8914@naver.com>
2018-11-16 00:47:12 +0900

    Add files via upload

---
+++ Algorithm/분산처리_1009.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+#include <ios>
+#include <math.h>
+
+using namespace std;
+
+/*
+원리
+53 mod 2 와(((5 mod 2) * 5 mod 2) * 5 mod 2)  는  같다는걸 이용
+*/
+int pow_mod(int x, int y, int p, int temp){
+	if (y == 1)return temp;
+	y--;
+	return pow_mod(x, y, p, (x*temp) % p);
+}
+int main() {
+	cin.tie(NULL);
+	cout.tie(NULL);
+	ios::sync_with_stdio(false);
+	int T = 0;
+	int a,b=0;
+	int result = 0;
+	cin >> T;
+	for (int i = 0; i < T; i++) {
+		cin >> a >> b;
+		result = pow_mod(a, b, 10, a % 10);
+		if (result == 0) result = 10;
+		cout << result<< "\n";
+	}
+	return 0;
+}
