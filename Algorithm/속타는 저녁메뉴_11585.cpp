72e7549a9650c7e61016bee737a0c0e8eab7fb8c
SANG WON <style8914@naver.com>
2018-12-05 01:29:13 +0900

    Add files via upload

---
+++ Algorithm/속타는 저녁메뉴_11585.cpp
@@ -0,0 +1,30 @@
+#include<iostream>
+
+using namespace std;
+
+#define MAX 1000005
+
+int N;
+int rear = 0;
+int front = 0;
+int menu[MAX];
+int current[MAX];
+
+void enQueue(char item){
+		rear = (rear + 1) % N;
+		menu[rear] = item;
+}
+
+int main() {
+	char item;
+	ios::sync_with_stdio(false);
+	cin >> N;
+	for (auto i = 0; i < N; i++) {
+		cin >> item;
+		enQueue(item);
+	}
+	for (auto i = 0; i < N; i++) {
+		cin >> current[i];
+	}
+	return 0;
+}
