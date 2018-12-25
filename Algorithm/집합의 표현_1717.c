5d3c57d63185daaabceba31c98e02dde36e455af
SANG WON <style8914@naver.com>
2018-12-26 01:33:49 +0900

    Add files via upload

---
+++ Algorithm/집합의 표현_1717.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+#include <string.h>
+int N[1000001];
+
+int findSet(int);
+void mergeSet(int, int);
+
+int main() {
+	int n, m;
+	scanf_s("%d %d", &n, &m);
+	memset(N, -1, sizeof(N));
+	for (int i = 0; i < m; ++i) {
+		int a, b, c;
+		scanf_s("%d %d %d", &c, &a, &b);
+		if (c == 0)
+			mergeSet(a, b);
+		else {
+			if (findSet(a) == findSet(b))
+				printf("YES\n");
+			else
+				printf("NO\n");
+		}
+	} return 0;
+}
+
+int findSet(int a) {
+	if (N[a] < 0)
+		return a;
+	
+	return N[a] = findSet(N[a]);
+}
+void mergeSet(int a, int b) {
+	a = findSet(a);//부모 판별
+	b = findSet(b);
+	if (a == b)
+		return;
+	N[b] = a;
+}
