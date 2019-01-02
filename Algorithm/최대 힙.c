e6e21574fc4b5629ac8603412293017bb8fbe3f9
SANG WON <style8914@naver.com>
2019-01-02 20:38:05 +0900

    Update 최대 힙.c

--- Algorithm/최대 힙.c
+++ Algorithm/최대 힙.c
@@ -56,7 +56,6 @@ void create() {
 
 void swap(int a, int b) {
 	int tmp = 0;
-	
 	tmp = tree[a];
 	tree[a] = tree[b];
 	tree[b] = tmp;
@@ -64,7 +63,6 @@ void swap(int a, int b) {
 
 void del() {
 	printf("%d\n", tree[1]);
-	
 	if (tree[1] != 0) {
 		tree[1] = tree[length];
 		tree[length] = 0;
