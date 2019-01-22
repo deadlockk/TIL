a953c01da4d4f70e2d215813c047fe4ddb582623
SANG WON <style8914@naver.com>
2019-01-23 01:44:17 +0900

    Add files via upload

---
+++ Algorithm/일곱 난쟁이_2309.cpp
@@ -0,0 +1,57 @@
+#include <iostream>
+#include <ios>
+#include <algorithm>
+#include<string.h>
+
+using namespace std;
+
+int height[10];
+int answer[8];
+void brute_force();
+
+int main() {
+	cin.tie(NULL);
+	cout.tie(NULL);
+	ios::sync_with_stdio(false);
+
+	for (int i = 0; i < 9; i++) {
+		cin >> height[i];
+	}
+	brute_force();
+
+	sort(answer, answer + 8);
+	for (int i = 1; i <= 7; i++) {
+		cout << answer[i]<<"\n";
+	}
+	return 0;
+}
+
+void brute_force() {
+	int sum = 0;
+	int a = 0;
+	int b = 1;
+	int counter = 0;
+
+	while (true) {
+		b = a + 1;
+		for (int i = b; i <= 8; i++) {
+			memset(answer, 0, sizeof(answer));
+			counter = 0;
+			sum = 0;
+			for (int j = 0; j < 9; j++) {
+				if (j != a && j != i) {
+					answer[counter] = height[j];
+					counter++;
+				}
+			}
+			for (int k = 0; k < 7; k++) {
+				sum += answer[k];
+			}
+			if (sum == 100) {
+				return;
+			}
+			b++;
+		}
+		a++;
+	}
+}
