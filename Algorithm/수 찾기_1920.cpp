12ffc7cbb998f37f9a0987326b6c0a9bf95e848a
SANG WON <style8914@naver.com>
2019-02-03 13:24:56 +0900

    Add files via upload

---
+++ Algorithm/수 찾기_1920.cpp
@@ -0,0 +1,45 @@
+#include <iostream>
+#include <ios>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+vector<int> A;
+
+int binary_search(int x) {
+	int left = 0, right = A.size()-1;
+
+	while (left <= right) {
+		int mid = (left + right) / 2;
+		if (A[mid] > x)
+			right = mid - 1;
+		else if (A[mid] < x)
+			left = mid + 1;
+		else {
+			return 1;
+		}
+	}
+	return 0;
+}
+
+int main() {
+	cin.tie(NULL);
+	cout.tie(NULL);
+	ios::sync_with_stdio(false);
+	int N, M, num;
+
+	//A vector 생성
+	cin >> N;
+	while (N--) {
+		cin >> num;
+		A.push_back(num);
+	}
+	sort(A.begin(), A.end());
+	//M개 수 입력
+	cin >> M;
+	for(int i=0;i<M;i++){
+		cin >> num;
+		cout << binary_search(num) << "\n";
+	}
+	return 0;
+}
