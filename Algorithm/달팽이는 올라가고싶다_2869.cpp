acd02644dca3d4cc44ec4229037bf98adce71cec
SANG WON <style8914@naver.com>
2019-02-05 15:30:12 +0900

    Add files via upload

---
+++ Algorithm/달팽이는 올라가고싶다_2869.cpp
@@ -0,0 +1,54 @@
+
+//이분탐색으로
+#include <iostream>
+#include <ios>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+int main() {
+	cin.tie(NULL);
+	cout.tie(NULL);
+	ios::sync_with_stdio(false);
+
+	int A, B, V;
+
+	cin >> A >> B >> V;
+
+	int L = 1, R = (V / (A - B));
+
+	while (L <= R) {
+		int mid = (L + R) / 2;
+		long long cnt = mid * (A - B) + A;
+		if (cnt >= V)
+			R = mid - 1;
+		else
+			L = mid + 1;
+	}
+
+	cout << L + 1 << "\n";
+
+	return 0;
+}
+
+//#include <iostream>
+//#include <ios>
+//
+//using namespace std;
+//
+//int main() {
+//	cin.tie(NULL);
+//	cout.tie(NULL);
+//	ios::sync_with_stdio(false);
+//
+//	int A, B, V, answer;
+//
+//	cin >> A >> B >> V;
+//	
+//	answer = (V - B-1) / (A - B) + 1;
+//
+//	cout << answer;
+//	
+//	return 0;
+//}
